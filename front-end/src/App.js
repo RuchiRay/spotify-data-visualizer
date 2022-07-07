fa651ce8944de246745563c2479d5da28b47ba60
Ruchi Ray <ruchiray18@gmail.com>
2022-07-08 01:43:25 +0530

    added routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
+import { BrowserRouter, Routes, Route ,useLocation} from "react-router-dom";
 function App() {
   const [token, setToken] = useState(null);
   const [profile, setProfile] = useState(null);
@@ -13,12 +14,19 @@ function App() {
     };
     catchErrors(fetchData());
   }, []);
-  console.log('access token',accessToken );
+  console.log("access token", accessToken);
   return (
-    <div className="App text-4xl text-red-600">
-      <a href="http://localhost:8000/login">log in to spotify</a>
-      <button onClick={logout}>log out</button>
-    </div>
+    // <div className="App text-4xl text-red-600">
+    //   <a href="http://localhost:8000/login">log in to spotify</a>
+    //   <button onClick={logout}>log out</button>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
